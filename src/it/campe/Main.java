7dc5a2cff10a24cec80859e11376d2135e8331a5
MamkinKinder <alex.fedkiv@gmail.com>
2021-10-24 13:00:48 +0300

    Task Complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,9 +3,7 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-      int a;
-      int b;
-      a = 10;
-      b = 20;
+      int a = 10;
+      int b = 20;
     }
 }
